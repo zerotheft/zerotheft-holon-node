065aef9c78298817980ea0062e879e9d3219a659
racetrack0616 <racetrack0616@gmail.com>
2021-08-20 14:48:04 +0545

    update the configs

--- app/services/holonsService.js
+++ app/services/holonsService.js
@@ -11,11 +11,23 @@ const getHolonsService = async () => {
   }
 }
 
+/**
+ * This method returns the holon information with holonID, address, whether a holon can be funded or not which is actually determined if holon has got a proxy wallet address to receive a funds.
+ * @returns {Object} json object with usefull holon information as: 
+ * address(holon address),
+ * holonID(Unique ID of a holon used to regsiter in the blockchain), 
+ * reportsPath(symplink of a reports)  
+ * canBeFunded(if holon has got a proxy wallet address to receive funds)
+ * */
 const getHolonInfo = async () => {
   const address = getStorageValues() && getStorageValues().address
   const holonres = await getHolonIdByAddress(address)
   const canBeFunded = getProxyHolonValues().proxy
-  const reportsPath = fs.realpathSync(`${config.APP_PATH}/zt_report/reports`).match(/public[^~]+/)[0]
+  const reportsDir = `${config.APP_PATH}/zt_report/reports`
+  let reportsPath = ""
+  if (fs.existsSync(reportsDir)) {
+    reportsPath = fs.realpathSync(`${config.APP_PATH}/zt_report/reports`).match(/public[^~]+/)[0]
+  }
   return {
     address,
     holonID: holonres.holonID,
