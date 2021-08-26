b2464368e995d7e718f724cdfaa2a050efaa5219
racetrack0616 <racetrack0616@gmail.com>
2021-08-26 13:55:46 +0545

    remove commented code

--- app.js
+++ app.js
@@ -45,25 +45,6 @@ app.use('/exports', serveIndex(path.join(APP_PATH, 'exports')))
 app.use('/public', express.static(path.join(APP_PATH, 'public'), { fallthrough: false }))
 app.use('/public', serveIndex(path.join(APP_PATH, 'public')))
 
-// app.use((req, res) => {
-//   res.status(404)
-
-//   // respond with html page
-//   if (req.accepts('html')) {
-//     res.render('404', { url: req.url })
-//     return
-//   }
-
-//   // respond with json
-//   if (req.accepts('json')) {
-//     res.send({ error: 'Not found' })
-//     return
-//   }
-
-//   // default to plain-text. send()
-//   res.type('txt').send('Not found')
-// })
-
 // health check route
 app.get('/healthcheck', (req, res, next) =>
   res.send({
