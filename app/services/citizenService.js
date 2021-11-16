fedb488180236f1bf551a87c3b2e27aea5dbb946
racetrack0616 <racetrack0616@gmail.com>
2021-11-16 13:37:13 +0545

    add endpoint for proposal rating

--- app/services/citizenService.js
+++ app/services/citizenService.js
@@ -1,15 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { citizenFeedback } = require('zerotheft-node-utils/contracts/proposals')
 const { getCitizen } = require('zerotheft-node-utils/contracts/citizens')
 
+/**
+ * Find citizen based on voter address
+ * @param {string} voter - voter's address
+ * @returns JSON object of citizen's info
+ */
 const getCitizenInfo = async voter => {
-  try {
-    const citizen = await getCitizen(voter)
-    if (!citizen) throw new Error('Citizen is not registered yet.')
-    return citizen
-  } catch (e) {
-    throw e
+  const citizen = await getCitizen(voter)
+  if (!citizen) {
+    throw new Error('Citizen is not registered yet.')
   }
+  return citizen
+}
+
+/**
+ * Search and return citizen's feedback to a specific proposal
+ * @param {string} citizenAddress - citizen's address
+ * @param {string} proposalID - proposal's ID
+ */
+const getCitizenProposalRating = async (citizenAddress, proposalID) => {
+  const response = await citizenFeedback(proposalID, citizenAddress)
+  return response
 }
 
 module.exports = {
   getCitizenInfo,
+  getCitizenProposalRating,
 }
